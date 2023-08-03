9ab507060ef272f8c080f8b90365fd3cbec1c063
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-08-03 23:34:49 +0200

    chore: api registration refactor

--- includes/wcoapi.php
+++ includes/wcoapi.php
@@ -1,11 +1,14 @@
 <?php
-	add_action('rest_api_init', function () {
-		register_rest_route('wcoapi/v1/orders', 'handle_response', array(
-			'method' => 'GET',
-			'callback' => 'handle_get_orders'
-		));
-	});
-
 	function handle_get_orders($request) {
 
 	}
+
+	function register_rest_apis () {
+		register_rest_route('wcoapi/v1/orders', 'handle_response', array(
+			'method' => 'GET',
+			'callback' => function ($request) { handle_get_orders($request); }
+		));
+	}
+
+	add_action('rest_api_init', 'register_rest_apis');
+
