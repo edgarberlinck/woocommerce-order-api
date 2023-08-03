418fc98c16085f56f98eb50f3465ee73705d9f39
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-08-03 22:47:10 +0200

    fix: Plugin PHP version requirements

--- woocommerce-order-api.php
+++ woocommerce-order-api.php
@@ -5,6 +5,8 @@ Plugin Name: Woocommerce Order Api
 Plugin URI: https://github.com/edgarberlinck/woocommerce-order-api.git
 Description: A brief description of the Plugin.
 Version: 1.0
+Requires at least: 7.0.0
+Requires PHP: 7.4.30
 Author: Edgar Muniz Berlinck
 Author URI: https://beacons.ai/edgarberlinck
 License: A "Slug" license name e.g. GPL2
