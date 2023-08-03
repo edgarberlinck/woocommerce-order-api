fecc0dbc670f4468f26e3e47537fb3056d78e14d
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-08-03 22:32:54 +0200

    Initial Import

---
+++ woocommerce-order-api.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Woocommerce Order Api
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Edgar Muniz Berlinck
+Author URI: https://beacons.ai/edgarberlinck
+License: A "Slug" license name e.g. GPL2
+*/
