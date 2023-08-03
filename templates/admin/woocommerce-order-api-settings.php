bda14e5da04b07e63453d535c9ce74e9eff04386
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-08-04 00:11:23 +0200

    feat: creating the settings tab

---
+++ templates/admin/woocommerce-order-api-settings.php
@@ -0,0 +1,4 @@
+<div id="woocommerce-order-api-settings">
+
+	<?php wp_nonce_field('wcoapi_woo_settings', '_wcoapinounce') ?>
+</div>
--- woocommerce-order-api.php
+++ woocommerce-order-api.php
@@ -32,7 +32,7 @@ class Woocommerce_Order_Api_Install {
 
 	protected static $instance = null;
 
-	public static function instance() {
+	public static function instance(): ?Woocommerce_Order_Api_Install {
 		if (is_null(self::$instance)) {
 			self::$instance = new self();
 		}
@@ -42,10 +42,38 @@ class Woocommerce_Order_Api_Install {
 
 	public function __construct() {
 		$this->include();
+		$this->create_menus();
 	}
 
 	public function include() {
 		require_once WOOCOMMERCE_ORDER_API_PATH . 'includes/wcoapi.php';
+	}
+
+	public function create_menus() {
+		add_filter('woocommerce_settings_tabs_array', [$this, 'add_settings_tab'], 50);
+		add_filter('woocommerce_settings_wcoapi', [$this, 'settings_page']);
+		add_filter('woocommerce_update_options_wcoapi', [$this, 'update_settings']);
+	}
+
+	function add_settings_tab($tabs) {
+		$tabs['wcoapi'] = esc_html('WC Order API', 'woocommerce-order-api');
+		return $tabs;
+	}
+
+	function settings_page() {
+		require_once WOOCOMMERCE_ORDER_API_PATH . 'templates/admin/woocommerce-order-api-settings.php';
+	}
+
+	function update_settings() {
+		if (empty($_POST['_wcoapinounce']) || !wp_verify_nonce($_POST['_wcoapinounce'], 'wcoapi_woo_settings')) {
+			return;
+		}
+//		Example data
+//		prefixes {
+//			order_paid_with_cash: 'U',
+//			order_paid_with_credit: 'K'
+//		}
+		update_option('wcoapi_data', $_POST['wcoapi_data']);
 	}
 }
 
