36cd81825a33aad50d50f299721a2b215db50da8
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2023-08-03 23:15:15 +0200

    feat: adding pluging instalation boilerplate feat: registering GET orders route

---
+++ includes/wcoapi.php
@@ -0,0 +1,11 @@
+<?php
+	add_action('rest_api_init', function () {
+		register_rest_route('wcoapi/v1/orders', 'handle_response', array(
+			'method' => 'GET',
+			'callback' => 'handle_get_orders'
+		));
+	});
+
+	function handle_get_orders() {
+
+	}
--- woocommerce-order-api.php
+++ woocommerce-order-api.php
@@ -11,3 +11,46 @@ Author: Edgar Muniz Berlinck
 Author URI: https://beacons.ai/edgarberlinck
 License: MIT
 */
+
+defined('ABSPATH') || exit;
+
+if (
+	!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins',
+	get_option('active_plugins')))
+) {
+	exit;
+}
+
+if (!defined('WOOCOMMERCE_ORDER_API_PATH')) {
+	define('WOOCOMMERCE_ORDER_API_PATH', plugin_dir_path(__FILE__));
+}
+if (!defined('WOOCOMMERCE_ORDER_API_DIR')) {
+	define('WOOCOMMERCE_ORDER_API_DER', __FILE__);
+}
+
+class Woocommerce_Order_Api_Install {
+
+	protected static $instance = null;
+
+	public static function instance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function __construct() {
+		$this->include();
+	}
+
+	public function include() {
+		require_once WOOCOMMERCE_ORDER_API_PATH . 'includes/wcoapi.php';
+	}
+}
+
+function init_plugin_woocommerce_order_api() {
+	Woocommerce_Order_Api_Install::instance();
+}
+
+add_action('plugin_loaded', 'init_plugin_woocommerce_order_api', 5);
